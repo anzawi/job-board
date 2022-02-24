52490efb2a8bc1a51695e329352121105e3859e0
mohammad walid anzawi <m.anzawi2013@gmail.com>
2022-02-24 10:10:10 +0200

    create data-context and migrations

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public sealed class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Job> Jobs { set; get; }
+        public DbSet<Application> Applications { set; get; }
+    }
+}
