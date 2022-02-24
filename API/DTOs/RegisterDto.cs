5bf2ab486c4dc7bc02d6a503166e2ebc4f695c39
mohammad walid anzawi <m.anzawi2013@gmail.com>
2022-02-24 10:29:28 +0200

    create DTOs models and token service

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] [EmailAddress] public string Email { set; get; }
+
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$",
+            ErrorMessage = "Password must contains capital and small letters, symbols, and numbers - length 4-8 characters.")]
+        public string Password { set; get; }
+
+        [Required] public string Username { set; get; }
+    }
+}
---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string Username { set; get; }
+        public string Token { set; get; }
+        public string CurrentPosition { set; get; }
+        public string Resume { set; get; }
+        public string Email { set; get; }
+        public bool IsAdmin { set; get; }
+    }
+}
