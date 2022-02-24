66c733008b22703c99a154ba93f55873f63161ba
mohammad walid anzawi <m.anzawi2013@gmail.com>
2022-02-24 12:23:41 +0200

    add logout

--- client/src/features/NavBar.tsx
+++ client/src/features/NavBar.tsx
@@ -97,7 +97,12 @@ function NavBar() {
                                                 </li>
                                             </>): null
                                     }
-                                    
+                                    <li>
+                                        <button
+                                            onClick={() => userStore.logout()}
+                                              className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">
+                                            Logout</button>
+                                    </li>
                                 </>
                             )
                         }
